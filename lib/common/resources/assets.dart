e29cc144290197d3b70a4aa96d42a899a3069349
Kishan-vitec <kishan.gondaliya@vitec-visual.com>
2025-11-05 09:27:55 +0530

    Auto-generated from Gemini: lib/common/resources/assets.dart

---
+++ lib/common/resources/assets.dart
@@ -0,0 +1,12 @@
+class Assets {
+  static const String logo = "https://i.imgur.com/835V0T3.png";
+  static const String hideIcon = "https://api.flutter.dev/assets/ui/icons/visibility_off-24px.svg";
+  static const String showIcon = "https://api.flutter.dev/assets/ui/icons/visibility-24px.svg";
+  static const String checkIcon = "https://api.flutter.dev/assets/ui/icons/check_circle-24px.svg";
+  static const String errorIcon = "https://api.flutter.dev/assets/ui/icons/error-24px.svg";
+  static const String calendarIcon = "https://api.flutter.dev/assets/ui/icons/calendar_today-24px.svg";
+  static const String accountIllustration = "https://i.imgur.com/2Y05X3g.png";
+  static const String checklistBg = "https://images.unsplash.com/photo-1542831371-29b0f74f9713?w=500&q=80";
+  static const String product = "https://i.imgur.com/YgY2ODf.png";
+  
+}
